0ca191e155d26442f6f173cf14ad1a30e39da918
aymanred121 <ayman20191700160@cis.asu.edu.eg>
2022-04-24 14:57:43 +0200

    Added missing input imgs transformed imgs' 2D Array into 1D Array

--- ParallelBGSubtraction/Main.cpp
+++ ParallelBGSubtraction/Main.cpp
@@ -8,7 +8,8 @@
 #using <System.Windows.Forms.dll>
 using namespace std;
 using namespace msclr::interop;
-
+#define MASK 294
+#define THRESHOLD 40
 int main()
 {
 	ImgProcessing imgProc;
@@ -16,18 +17,22 @@ int main()
 	System::String^ imagePath;
 	int ImageWidth = 4, ImageHeight = 4;
 	int imageCount = 0;
-	int threshold = 40; // Controls what pixels to show in FG mask
-	int** imagesData; // 2D matrix to hold all the image vectors
-
+	int* imagesData; // 2D matrix to hold all the image vectors
 	std::string* fileList = fManager.getFiles("..//Data//Input", &imageCount);
-	imagesData = new int* [imageCount];
+	imagesData = new int[320 * 240 * imageCount];
 
 	// Load each image in the Input directory as a 1D Vector and save it in a 2D matrix
 	for (int i = 0; i < imageCount; i++)
 	{
 		// Seperate imageCount with count of processors with scatter/bcast
 		imagePath = marshal_as<System::String^>(fileList[i]);
-		imagesData[i] = imgProc.inputImage(&ImageWidth, &ImageHeight, imagePath);
+		int* input = imgProc.inputImage(&ImageWidth, &ImageHeight, imagePath);
+		//put all imgs array into input1d array such tath input1d[i] is the i-th img
+		for (int j = 0; j < ImageHeight * ImageWidth; j++)
+		{
+			imagesData[i * ImageHeight * ImageWidth + j] = input[j];
+		}
+		delete[]input;
 		std::cout << fileList[i] << std::endl;
 	}
 
@@ -37,29 +42,23 @@ int main()
 	// Calculate background pixels with mean
 	for (int p = 0; p < ImageHeight * ImageWidth; p++)
 	{
-		// Convert to 1D array for MPI
+		// Convert to 1D array for MPI		
 		int sum = 0;
 		for (int i = 0; i < imageCount; i++)
-			sum += imagesData[i][p];
+		{
+			sum += imagesData[i * ImageHeight * ImageWidth + p];
+		}
 		BG_Pixels[p] = sum / imageCount;
+		FG_Pixels[p] = (std::abs(BG_Pixels[p] - imagesData[(MASK) * (ImageHeight * ImageWidth) + p]) > THRESHOLD ? 255 : 0);
+
 	}
 
 	// Calculate the foreground mask pixels
-	for (int p = 0; p < ImageHeight * ImageWidth; p++)
-	{
-		// Joing with above loop
-		FG_Pixels[p] = (std::abs(BG_Pixels[p] - imagesData[imageCount - 1][p]) > threshold ? 255 : 0);
-	}
 	// Create foreground mask and background images
 	imgProc.createImage(FG_Pixels, ImageWidth, ImageHeight, 0);
 	imgProc.createImage(BG_Pixels, ImageWidth, ImageHeight, 1);
-	
-	for (int i = 0; i < imageCount; i++)
-	{
-		free(imagesData[i]);
-	}
-	
-	//system("pause");
+	delete[] imagesData;
+	system("pause");
 	return 0;
 
 }
